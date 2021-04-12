7659d99f31bd076232a5448ed80dba63dc547ac9
Laugh <laugh12321@vip.qq.com>
2021-04-12 22:38:04 +0800

    :sparkles: Add GUI

---
+++ about.py
@@ -0,0 +1,22 @@
+###########################################################################################
+###                        CODE:       WRITTEN BY: ANJAL.P AUGUST 11 2020               ###
+###                        PROJECT:    PELLIS Z1                                        ###
+###                        PURPOSE:    WINDOWS/LINUX/MAC OS FLAT MODERN UI              ###
+###                                    BASED ON QT DESIGNER, PySide2                    ###
+###                        USE CASE:   TEMPLATE FOR SOFTWARES                           ###
+###                        LICENCE:    MIT OPENSOURCE LICENCE                           ###
+###                                                                                     ###
+###                            CODE IS FREE TO USE AND MODIFY                           ###
+###########################################################################################
+
+
+# THIS FILE IS DEDICATED FOR THE ABOUT PAGE IN THE APPLICATION WHICH ACTS AS A OR AS A HELP TAB OF THE SOFTWARE APPLICATION
+#BELOW IS A DUMM RANDOM GENERATED TEXT TO DISPLAY IN THE ABOUT HOME PAGE.
+#KEEP ALL YOUR ABOUT IN ONE PLACE, ASSIGN IT TO A VARIABLE AND THEN IMPORT THIS FILE TO MAIN.PY FILE, AN DUSE THIS VARIABLE FOR THE DOCUMENT.
+aboutHome = """
+&nbsp;&nbsp;&nbsp;&nbsp;Click the <b>Upload Image</b> button to select the license plate to be detected, and click <b>Recognition</b> to detect.
+<br>
+<div align=center> 
+    <img width="540" src='icons/demo.png'>
+</div>
+"""
---
+++ ui_function.py
@@ -0,0 +1,241 @@
+###########################################################################################
+###                        CODE:       WRITTEN BY: ANJAL.P AUGUST 11 2020               ###
+###                        PROJECT:    PELLIS Z1                                        ###
+###                        PURPOSE:    WINDOWS/LINUX/MAC OS FLAT MODERN UI              ###
+###                                    BASED ON QT DESIGNER, PySide2                    ###
+###                        USE CASE:   TEMPLATE FOR SOFTWARES                           ###
+###                        LICENCE:    MIT OPENSOURCE LICENCE                           ###
+###                                                                                     ###
+###                            CODE IS FREE TO USE AND MODIFY                           ###
+###########################################################################################
+
+###########################################################################################
+#                                     DOCUMENTATION                                       #
+#                                                                                         #
+#                                                                                         #
+#  Each line of the code described below is commented well, such as: the purpose of the   #
+#  code, its function, returns e.t.c as in certain caes: the alternatives to that solul-  #
+#  ution, other sources like included PDF document has also the working of the code.      #
+#  CSS stylesheet of the buttons are given seperatly in the CSS.txt in the parent folder  #
+###########################################################################################
+
+###########################################################################################
+#                                       CAUTION                                           #
+#  SINCE MOST OF THE WORK IS DONE IN THE QT DESIGNER, YOU WAY NOT SEE THE STYLESHEET HERE #
+#  FOR THAT PLEASE REFER THE CSS.txt FILE PROVIDED IN THIS SAME FILE.                     #
+#  ALSO AMNY OF THE SETTINGS IS PREDEFINED IN THE QT DESIGNER ITSELF, SO HERE IN THIS FUN-#
+#  CTION WHAY HAPPENS AFTER THIS I.E. WHEN THE USER CHANGES THE INPUT STATE, ONLY IS DELT #
+#  HERE, SO IF YOU WANT TO MODIFY THE FILE, PLEASE OPEN THE CORRESPONDING .ui FILE IN QT  #
+#  DESIGNER AND MADE THE MODIFICATION AND THENY COME BACK HERE TO ADD FUNCTIONALITY TO THE#
+#  CHANGES.                                                                               #
+###########################################################################################
+import os
+import cv2
+from PySide2.QtGui import QImage, QIcon
+from PySide2.QtCore import Qt, QEvent, QEasingCurve, QTimer, QPropertyAnimation
+from PySide2.QtWidgets import QFileDialog, QFrame
+from detect import *
+from main import *
+from about import *
+
+GLOBAL_STATE = 0 #NECESSERY FOR CHECKING WEATHER THE WINDWO IS FULL SCREEN OR NOT
+GLOBAL_TITLE_BAR = True #NECESSERY FOR CHECKING WEATHER THE WINDWO IS FULL SCREEN OR NOT
+init = False # NECRESSERY FOR INITITTION OF THE WINDOW.
+
+# THIS CLASS HOUSES ALL FUNCTION NECESSERY FOR OUR PROGRAMME TO RUN.
+class UIFunction(MainWindow):
+
+    #----> INITIAL FUNCTION TO LOAD THE FRONT STACK WIDGET AND TAB BUTTON I.E. HOME PAGE 
+    #INITIALISING THE WELCOME PAGE TO: HOME PAGE IN THE STACKEDWIDGET, SETTING THE BOTTOM LABEL AS THE PAGE NAME, SETTING THE BUTTON STYLE.
+    def initStackTab(self):
+        global init
+        if init==False:
+            self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+            self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)")
+            init = True
+    ################################################################################################
+
+
+    #------> SETING THE APPLICATION NAME IN OUR CUSTOME MADE TAB, WHERE LABEL NAMED: lab_appname()
+    def labelTitle(self, appName):
+        self.ui.lab_appname.setText(appName)
+    ################################################################################################
+
+
+    #----> MAXIMISE/RESTORE FUNCTION
+    #THIS FUNCTION MAXIMISES OUR MAINWINDOW WHEN THE MAXIMISE BUTTON IS PRESSED OR IF DOUBLE MOUSE LEFT PRESS IS DOEN OVER THE TOPFRMAE.
+    #THIS MAKE THE APPLICATION TO OCCUPY THE WHOLE MONITOR.
+    def maximize_restore(self):
+        global GLOBAL_STATE
+        status = GLOBAL_STATE
+        if status == 0:
+            self.showMaximized()
+            GLOBAL_STATE = 1
+            self.ui.bn_max.setToolTip("Restore") 
+            self.ui.bn_max.setIcon(QIcon("icons/1x/restore.png")) #CHANGE THE MAXIMISE ICON TO RESTOR ICON
+            self.ui.frame_drag.hide() #HIDE DRAG AS NOT NECESSERY
+        else:
+            GLOBAL_STATE = 0
+            self.showNormal()
+            self.resize(self.width()+1, self.height()+1)
+            self.ui.bn_max.setToolTip("Maximize")
+            self.ui.bn_max.setIcon(QIcon("icons/1x/max.png")) #CHANGE BACK TO MAXIMISE ICON
+            self.ui.frame_drag.show()
+    ################################################################################################
+
+
+    #----> RETURN STATUS MAX OR RESTROE
+    #NECESSERY OFR THE MAXIMISE FUNCTION TRO WORK.
+    def returStatus():
+        return GLOBAL_STATE
+
+
+    def setStatus(status):
+        global GLOBAL_STATE
+        GLOBAL_STATE = status
+
+
+    #------> TOODLE MENU FUNCTION
+    #THIS FUNCTION TOODLES THE MENU BAR TO DOUBLE THE LENGTH OPENING A NEW ARE OF ABOUT TAB IN FRONT.
+    #ASLO IT SETS THE ABOUT>HOME AS THE FIRST PAGE.
+    #IF THE PAGE IS IN THE ABOUT PAGE THEN PRESSING AGAIN WILL RESULT IN UNDOING THE PROCESS AND COMMING BACK TO THE 
+    #HOME PAGE.
+    def toodleMenu(self, maxWidth, clicked):
+
+        #------> THIS LINE CLEARS THE BG OF PREVIOUS TABS : I.E. MAKING THEN NORMAL COLOR THAN LIGHTER COLOR.
+        for each in self.ui.frame_bottom_west.findChildren(QFrame): 
+            each.setStyleSheet("background:rgb(51,51,51)")
+
+        if clicked:
+            currentWidth = self.ui.frame_bottom_west.width() #Reads the current width of the frame
+            minWidth = 80 #MINIMUN WITDTH OF THE BOTTOM_WEST FRAME
+            if currentWidth==80:
+                extend = maxWidth
+                #----> MAKE THE STACKED WIDGET PAGE TO ABOUT HOME PAGE
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_about_home)
+                self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)")
+            else:
+                extend = minWidth
+                #-----> REVERT THE ABOUT HOME PAGE TO NORMAL HOME PAGE
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+                self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)")
+            #THIS ANIMATION IS RESPONSIBLE FOR THE TOODLE TO MOVE IN A SOME FIXED STATE.
+            self.animation = QPropertyAnimation(self.ui.frame_bottom_west, b"minimumWidth")
+            self.animation.setDuration(300)
+            self.animation.setStartValue(minWidth)
+            self.animation.setEndValue(extend)
+            self.animation.setEasingCurve(QEasingCurve.InOutQuart)
+            self.animation.start()
+    ################################################################################################
+
+
+    #-----> DEFAULT ACTION FUNCTION
+    def constantFunction(self):
+        #-----> DOUBLE CLICK RESULT IN MAXIMISE OF WINDOW
+        def maxDoubleClick(stateMouse):
+            if stateMouse.type() == QEvent.MouseButtonDblClick:
+                QTimer.singleShot(250, lambda: UIFunction.maximize_restore(self))
+
+        #----> REMOVE NORMAL TITLE BAR 
+        if True:
+            self.setWindowFlags(Qt.FramelessWindowHint)
+            self.setAttribute(Qt.WA_TranslucentBackground)
+            self.ui.frame_appname.mouseDoubleClickEvent = maxDoubleClick
+        else:
+            self.ui.frame_close.hide()
+            self.ui.frame_max.hide()
+            self.ui.frame_min.hide()
+            self.ui.frame_drag.hide()
+
+        #-----> RESIZE USING DRAG                                       THIS CODE TO DRAG AND RESIZE IS IN PROTOPYPE.
+        #self.sizegrip = QSizeGrip(self.ui.frame_drag)
+        #self.sizegrip.setStyleSheet("width: 20px; height: 20px; margin 0px; padding: 0px;")
+
+        #SINCE THERE IS NO WINDOWS TOPBAR, THE CLOSE MIN, MAX BUTTON ARE ABSENT AND SO THERE IS A NEED FOR THE ALTERNATIVE BUTTONS IN OUR
+        #DIALOG BOX, WHICH IS CARRIED OUT BY THE BELOW CODE
+        #-----> MINIMIZE BUTTON FUNCTION 
+        self.ui.bn_min.clicked.connect(lambda: self.showMinimized())
+
+        #-----> MAXIMIZE/RESTORE BUTTON FUNCTION
+        self.ui.bn_max.clicked.connect(lambda: UIFunction.maximize_restore(self))
+
+        #-----> CLOSE APPLICATION FUNCTION BUTTON
+        self.ui.bn_close.clicked.connect(lambda: self.close())
+    ################################################################################################################
+
+
+    #----> BUTTON IN TAB PRESSED EXECUTES THE CORRESPONDING PAGE IN STACKEDWIDGET PAGES
+    def buttonPressed(self, buttonName):
+
+        index = self.ui.stackedWidget.currentIndex()
+
+        #------> THIS LINE CLEARS THE BG OF PREVIOUS TABS I.E. FROM THE LITER COLOR TO THE SAME BG COLOR I.E. TO CHANGE THE HIGHLIGHT.
+        for each in self.ui.frame_bottom_west.findChildren(QFrame):
+            each.setStyleSheet("background:rgb(51,51,51)")
+
+        if buttonName=='bn_home':
+            if self.ui.frame_bottom_west.width()==80  and index!=0:
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_home)
+                self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)") # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST 
+
+            elif self.ui.frame_bottom_west.width()==160  and index!=1:  # ABOUT PAGE STACKED WIDGET
+                self.ui.stackedWidget.setCurrentWidget(self.ui.page_about_home)
+                self.ui.frame_home.setStyleSheet("background:rgb(91,90,90)") # SETS THE BACKGROUND OF THE CLICKED BUTTON TO LITER COLOR THAN THE REST
+
+        #ADD ANOTHER ELIF STATEMENT HERE FOR EXECTUITING A NEW MENU BUTTON STACK PAGE.
+    ########################################################################################################################
+
+
+    #----> STACKWIDGET EACH PAGE FUNCTION PAGE FUNCTIONS
+    # CODE TO PERFOMR THE TASK IN THE STACKED WIDGET PAGE 
+    # WHAT EVER WIDGET IS IN THE STACKED PAGES ITS ACTION IS EVALUATED HERE AND THEN THE REST FUNCTION IS PASSED.
+    def stackPage(self):
+
+        ######### PAGE_HOME ############# BELOW DISPLAYS THE FUNCTION OF WIDGET, LABEL, PROGRESS BAR, E.T.C IN STACKEDWIDGET page_HOME
+        self.ui.bn_home_image.clicked.connect(lambda: APFunction.getImage(self))
+        self.ui.bn_home_rec.clicked.connect(lambda: APFunction.ImageRec(self))
+
+        ##########PAGE: ABOUT HOME #############
+        self.ui.text_about_home.setVerticalScrollBar(self.ui.vsb_about_home)
+        self.ui.text_about_home.setText(aboutHome)
+    ################################################################################################################################
+
+    
+#------> CLASS WHERE ALL THE ACTION OF TH SOFTWARE IS PERFORMED:
+# THIS CLASS IS WHERE THE APPLICATION OF THE UI OR THE BRAINOF THE SOFTWARE GOES
+# UNTILL NOW WE SEPCIFIED THE BUTTON CLICKS, SLIDERS, E.T.C WIDGET, WHOSE APPLICATION IS EXPLORED HERE. THOSE FUNCTION WHEN DONE IS 
+# REDIRECTED TO THIS AREA FOR THE PROCESSING AND THEN THE RESULT ARE EXPOTED.
+#REMEMBER THE SOFTWARE UI HAS A FUNCTION WHOSE CODE SHOULD BE HERE    
+
+class APFunction():
+#-----> FUNCTION TO Get Image
+    def getImage(self):
+        
+        self.file_dir_temp, _ = QFileDialog.getOpenFileName(self, "选择被检测的车辆", "D:", ("图片文件(*.png *.jpg *.jpeg)"))
+        self.file_dir = self.file_dir_temp.replace("\\","/")
+
+        image = cv2.imread(self.file_dir)
+        rows, cols, channels = image.shape
+        bytesPerLine = channels * cols
+        cv2.cvtColor(image, cv2.COLOR_BGR2RGB, image)
+        QImg = QImage(image.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+        self.ui.lab_home_main_disc.setPixmap(QPixmap.fromImage(QImg).scaled(self.ui.lab_home_main_disc.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+        # except Exception as e:
+        #     self.errorexec("请加载图片", "icons/1x/errorAsset 55.png", "Retry")
+
+#-----> FUNCTION TO Get Image
+    def ImageRec(self):
+
+        output_dir, _ = os.path.split(self.file_dir)
+        output_file = detect(source=self.file_dir, output=output_dir)
+
+        image = cv2.imread(output_file)
+        rows, cols, channels = image.shape
+        bytesPerLine = channels * cols
+        cv2.cvtColor(image, cv2.COLOR_BGR2RGB, image)
+        QImg = QImage(image.data, cols, rows, bytesPerLine, QImage.Format_RGB888)
+        self.ui.lab_home_main_disc.setPixmap(QPixmap.fromImage(QImg).scaled(self.ui.lab_home_main_disc.size(), Qt.KeepAspectRatio, Qt.SmoothTransformation))
+
+        
+###############################################################################################################################################################
